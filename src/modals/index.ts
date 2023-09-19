ae63a2f302591f662053cac9944c6a0c1887c1b5
Tommy <tommydanks2000@outlook.com>
2023-09-19 07:14:37 +0100

    remove news info modal from modals/index

--- src/modals/index.ts
+++ src/modals/index.ts
@@ -4,10 +4,8 @@ import TrailerModal from './TrailerModal';
 import VideoSettingsModal from './VideoSettingsModal';
 import VideoEpisodesModal from './VideoEpisodesModal';
 import NotificationsModal from './NotificationsModal';
-import NewisInfoModal from './NewsInfoModal';
 
 export {
-  NewisInfoModal,
   NotificationsModal,
   EpisodesModal,
   VideoSettingsModal,
