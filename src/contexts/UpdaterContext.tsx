6cf0eece23b1153bc014772c3d4eb1b8afac115e
Tommy <tommydanks2000@outlook.com>
2023-10-23 15:15:45 +0100

    (update): add types/ needupdateapp call

--- src/contexts/UpdaterContext.tsx
+++ src/contexts/UpdaterContext.tsx
@@ -1,7 +1,7 @@
 import {createContext, useEffect, useState} from 'react';
+import {Alert} from 'react-native';
 import Toast from 'react-native-toast-message';
 
-//@ts-ignore
 import * as UpdateAPK from 'rn-update-apk';
 
 export const UpdaterContext = createContext<
@@ -46,6 +46,23 @@ export const UpdaterProvider = ({children}: Props) => {
         position: 'top',
         text1: 'Auto-update Faield',
       });
+    },
+    needUpdateApp: performUpdate => {
+      // TODO: create a custom Alert
+      Alert.alert(
+        'Update Available',
+        'New version released, do you want to update?',
+        [
+          {text: 'Cancel', onPress: () => {}},
+          // Note, apps can be large. You may want to check if the network is metered (cellular data) to be nice.
+          // Note that the user will likely get a popup saying the device is set to block installs from uknown sources.
+          // ...you will need to guide them through that, maybe by explaining it here, before you call performUpdate(true);
+          {text: 'Update', onPress: () => performUpdate(true)},
+        ],
+        {
+          userInterfaceStyle: 'dark',
+        },
+      );
     },
   });
 
